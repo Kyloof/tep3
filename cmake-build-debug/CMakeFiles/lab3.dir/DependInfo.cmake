3bd1f131f69396c678b19c791b6bd24d3bdacd72
Kyloof <jankubow.sr@gmail.com>
2024-11-25 18:00:02 +0100

    beta

---
+++ cmake-build-debug/CMakeFiles/lab3.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/janku/OneDrive/Desktop/Studia/TEP/tep3/main.cpp" "CMakeFiles/lab3.dir/main.cpp.obj" "gcc" "CMakeFiles/lab3.dir/main.cpp.obj.d"
+  "C:/Users/janku/OneDrive/Desktop/Studia/TEP/tep3/src/AbstractSyntaxTree/AbstractSyntaxTree.cpp" "CMakeFiles/lab3.dir/src/AbstractSyntaxTree/AbstractSyntaxTree.cpp.obj" "gcc" "CMakeFiles/lab3.dir/src/AbstractSyntaxTree/AbstractSyntaxTree.cpp.obj.d"
+  "C:/Users/janku/OneDrive/Desktop/Studia/TEP/tep3/src/Command/Command.cpp" "CMakeFiles/lab3.dir/src/Command/Command.cpp.obj" "gcc" "CMakeFiles/lab3.dir/src/Command/Command.cpp.obj.d"
+  "C:/Users/janku/OneDrive/Desktop/Studia/TEP/tep3/src/NodeTypes/Literal/Literal.cpp" "CMakeFiles/lab3.dir/src/NodeTypes/Literal/Literal.cpp.obj" "gcc" "CMakeFiles/lab3.dir/src/NodeTypes/Literal/Literal.cpp.obj.d"
+  "C:/Users/janku/OneDrive/Desktop/Studia/TEP/tep3/src/NodeTypes/Operator/AOperator.cpp" "CMakeFiles/lab3.dir/src/NodeTypes/Operator/AOperator.cpp.obj" "gcc" "CMakeFiles/lab3.dir/src/NodeTypes/Operator/AOperator.cpp.obj.d"
+  "C:/Users/janku/OneDrive/Desktop/Studia/TEP/tep3/src/NodeTypes/Variable/Variable.cpp" "CMakeFiles/lab3.dir/src/NodeTypes/Variable/Variable.cpp.obj" "gcc" "CMakeFiles/lab3.dir/src/NodeTypes/Variable/Variable.cpp.obj.d"
+  "C:/Users/janku/OneDrive/Desktop/Studia/TEP/tep3/src/OperatorType/BinaryOperator/ABinaryOperator.cpp" "CMakeFiles/lab3.dir/src/OperatorType/BinaryOperator/ABinaryOperator.cpp.obj" "gcc" "CMakeFiles/lab3.dir/src/OperatorType/BinaryOperator/ABinaryOperator.cpp.obj.d"
+  "C:/Users/janku/OneDrive/Desktop/Studia/TEP/tep3/src/OperatorType/UniOperator/AUniOperator.cpp" "CMakeFiles/lab3.dir/src/OperatorType/UniOperator/AUniOperator.cpp.obj" "gcc" "CMakeFiles/lab3.dir/src/OperatorType/UniOperator/AUniOperator.cpp.obj.d"
+  "C:/Users/janku/OneDrive/Desktop/Studia/TEP/tep3/src/OperatorsFunc/BinaryOperatorsFunc/Addition/Addition.cpp" "CMakeFiles/lab3.dir/src/OperatorsFunc/BinaryOperatorsFunc/Addition/Addition.cpp.obj" "gcc" "CMakeFiles/lab3.dir/src/OperatorsFunc/BinaryOperatorsFunc/Addition/Addition.cpp.obj.d"
+  "C:/Users/janku/OneDrive/Desktop/Studia/TEP/tep3/src/OperatorsFunc/BinaryOperatorsFunc/Divide/Divide.cpp" "CMakeFiles/lab3.dir/src/OperatorsFunc/BinaryOperatorsFunc/Divide/Divide.cpp.obj" "gcc" "CMakeFiles/lab3.dir/src/OperatorsFunc/BinaryOperatorsFunc/Divide/Divide.cpp.obj.d"
+  "C:/Users/janku/OneDrive/Desktop/Studia/TEP/tep3/src/OperatorsFunc/BinaryOperatorsFunc/Multiply/Multiply.cpp" "CMakeFiles/lab3.dir/src/OperatorsFunc/BinaryOperatorsFunc/Multiply/Multiply.cpp.obj" "gcc" "CMakeFiles/lab3.dir/src/OperatorsFunc/BinaryOperatorsFunc/Multiply/Multiply.cpp.obj.d"
+  "C:/Users/janku/OneDrive/Desktop/Studia/TEP/tep3/src/OperatorsFunc/BinaryOperatorsFunc/Substract/Subtract.cpp" "CMakeFiles/lab3.dir/src/OperatorsFunc/BinaryOperatorsFunc/Substract/Subtract.cpp.obj" "gcc" "CMakeFiles/lab3.dir/src/OperatorsFunc/BinaryOperatorsFunc/Substract/Subtract.cpp.obj.d"
+  "C:/Users/janku/OneDrive/Desktop/Studia/TEP/tep3/src/OperatorsFunc/UniOperatorsFunc/Cos/Cos.cpp" "CMakeFiles/lab3.dir/src/OperatorsFunc/UniOperatorsFunc/Cos/Cos.cpp.obj" "gcc" "CMakeFiles/lab3.dir/src/OperatorsFunc/UniOperatorsFunc/Cos/Cos.cpp.obj.d"
+  "C:/Users/janku/OneDrive/Desktop/Studia/TEP/tep3/src/OperatorsFunc/UniOperatorsFunc/Sinus/Sinus.cpp" "CMakeFiles/lab3.dir/src/OperatorsFunc/UniOperatorsFunc/Sinus/Sinus.cpp.obj" "gcc" "CMakeFiles/lab3.dir/src/OperatorsFunc/UniOperatorsFunc/Sinus/Sinus.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/lab3.dir/cmake_clean.cmake
@@ -0,0 +1,39 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab3.dir/main.cpp.obj"
+  "CMakeFiles/lab3.dir/main.cpp.obj.d"
+  "CMakeFiles/lab3.dir/src/AbstractSyntaxTree/AbstractSyntaxTree.cpp.obj"
+  "CMakeFiles/lab3.dir/src/AbstractSyntaxTree/AbstractSyntaxTree.cpp.obj.d"
+  "CMakeFiles/lab3.dir/src/Command/Command.cpp.obj"
+  "CMakeFiles/lab3.dir/src/Command/Command.cpp.obj.d"
+  "CMakeFiles/lab3.dir/src/NodeTypes/Literal/Literal.cpp.obj"
+  "CMakeFiles/lab3.dir/src/NodeTypes/Literal/Literal.cpp.obj.d"
+  "CMakeFiles/lab3.dir/src/NodeTypes/Operator/AOperator.cpp.obj"
+  "CMakeFiles/lab3.dir/src/NodeTypes/Operator/AOperator.cpp.obj.d"
+  "CMakeFiles/lab3.dir/src/NodeTypes/Variable/Variable.cpp.obj"
+  "CMakeFiles/lab3.dir/src/NodeTypes/Variable/Variable.cpp.obj.d"
+  "CMakeFiles/lab3.dir/src/OperatorType/BinaryOperator/ABinaryOperator.cpp.obj"
+  "CMakeFiles/lab3.dir/src/OperatorType/BinaryOperator/ABinaryOperator.cpp.obj.d"
+  "CMakeFiles/lab3.dir/src/OperatorType/UniOperator/AUniOperator.cpp.obj"
+  "CMakeFiles/lab3.dir/src/OperatorType/UniOperator/AUniOperator.cpp.obj.d"
+  "CMakeFiles/lab3.dir/src/OperatorsFunc/BinaryOperatorsFunc/Addition/Addition.cpp.obj"
+  "CMakeFiles/lab3.dir/src/OperatorsFunc/BinaryOperatorsFunc/Addition/Addition.cpp.obj.d"
+  "CMakeFiles/lab3.dir/src/OperatorsFunc/BinaryOperatorsFunc/Divide/Divide.cpp.obj"
+  "CMakeFiles/lab3.dir/src/OperatorsFunc/BinaryOperatorsFunc/Divide/Divide.cpp.obj.d"
+  "CMakeFiles/lab3.dir/src/OperatorsFunc/BinaryOperatorsFunc/Multiply/Multiply.cpp.obj"
+  "CMakeFiles/lab3.dir/src/OperatorsFunc/BinaryOperatorsFunc/Multiply/Multiply.cpp.obj.d"
+  "CMakeFiles/lab3.dir/src/OperatorsFunc/BinaryOperatorsFunc/Substract/Subtract.cpp.obj"
+  "CMakeFiles/lab3.dir/src/OperatorsFunc/BinaryOperatorsFunc/Substract/Subtract.cpp.obj.d"
+  "CMakeFiles/lab3.dir/src/OperatorsFunc/UniOperatorsFunc/Cos/Cos.cpp.obj"
+  "CMakeFiles/lab3.dir/src/OperatorsFunc/UniOperatorsFunc/Cos/Cos.cpp.obj.d"
+  "CMakeFiles/lab3.dir/src/OperatorsFunc/UniOperatorsFunc/Sinus/Sinus.cpp.obj"
+  "CMakeFiles/lab3.dir/src/OperatorsFunc/UniOperatorsFunc/Sinus/Sinus.cpp.obj.d"
+  "lab3.exe"
+  "lab3.exe.manifest"
+  "lab3.pdb"
+  "liblab3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
